55c5851d4eef3f0a9af2dacd88d520bf5509c914
Anton Zalesky <azalesky@by.byelex.com>
2018-07-19 11:27:25 +0300

    Add mainnet

--- truffle.js
+++ truffle.js
@@ -14,6 +14,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: 4
+    },
+    mainnet: {
+      host: "localhost",
+      port: 8545,
+      from: "0x5b0d2b79010d1948ed475bfb507da5d570354c61",
+      network_id: 1,
+      gasPrice: 10000000000
     }
   }
 };
